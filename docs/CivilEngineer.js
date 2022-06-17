669b06aad1caa7ae90f26bc03ddcd7cc2f6f4a7d
jieshenlee <102533538+jieshenlee@users.noreply.github.com>
2022-06-17 11:00:48 +0530

    Add files via upload

---
+++ docs/CivilEngineer.js
@@ -0,0 +1,60 @@
+function mouseEnter1() { document.getElementById("demo").style.color = "white";
+}
+function mouseLeave1() { document.getElementById("demo").style.color = "black";
+}
+function mouseEnterIntroduction() { document.getElementById("introduction").style.color = "#ffb366";
+}
+function mouseLeaveIntroduction() { document.getElementById("introduction").style.color = "black";
+}
+function mouseEnterRoles() { document.getElementById("roles").style.color = "white";
+}
+function mouseLeaveRoles() { document.getElementById("roles").style.color = "black";
+}
+function mouseEnterSkills() { document.getElementById("skills").style.color = "white";
+}
+function mouseLeaveSkills() { document.getElementById("skills").style.color = "black";
+}
+function mouseEnterFuture() { document.getElementById("future").style.color = "white";
+}
+function mouseLeaveFuture() { document.getElementById("future").style.color = "black";
+}
+function mouseEnterPathway() { document.getElementById("pathway").style.color = "#ffb366";
+}
+function mouseLeavePathway() { document.getElementById("pathway").style.color = "black";
+}
+function mouseEnterAbout() { document.getElementById("about").style.color = "white";
+}
+function mouseLeaveAbout() { document.getElementById("about").style.color = "black";
+}
+function mouseEnterCivil() { document.getElementById("ParaCivil").style.color = "#ffb366";
+}
+function mouseLeaveCivil() { document.getElementById("ParaCivil").style.color = "black";
+}
+function mouseEnterQs() { document.getElementById("ParaQs").style.color = "#ffb366";
+}
+function mouseLeaveQs() { document.getElementById("ParaQs").style.color = "black";
+}
+function mouseEnterPm() { document.getElementById("ParaPm").style.color = "#ffb366";
+}
+function mouseLeavePm() { document.getElementById("ParaPm").style.color = "black";
+}
+function mouseEnterCad() { document.getElementById("ParaCad").style.color = "#ffb366";
+}
+function mouseLeaveCad() { document.getElementById("ParaCad").style.color = "black";
+}
+function mouseEnterRevitt() { document.getElementById("ParaRevitt").style.color = "#ffb366";
+}
+function mouseLeaveRevitt() { document.getElementById("ParaRevitt").style.color = "black";
+}
+function mouseEnterProject() { document.getElementById("ParaProject").style.color = "#ffb366";
+}
+function mouseLeaveProject() { document.getElementById("ParaProject").style.color = "black";
+}
+function mouseEnterPrim() { document.getElementById("ParaPrim").style.color = "#ffb366";
+}
+function mouseLeavePrim() { document.getElementById("ParaPrim").style.color = "black";
+}
+function mouseEnterIntro() { document.getElementById("ParaIntro").style.color = "#ffb366";
+}
+function mouseLeaveIntro() { document.getElementById("ParaIntro").style.color = "black";
+}
